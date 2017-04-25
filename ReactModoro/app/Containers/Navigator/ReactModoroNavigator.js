4c4db44827ed47ad027040758ddb9370d82b4053
Kevin <kjs2b@virginia.edu>
2017-04-25 14:36:44 -0700

    Added first component SplashContainer

--- ReactModoro/app/Containers/Navigator/ReactModoroNavigator.js
+++ ReactModoro/app/Containers/Navigator/ReactModoroNavigator.js
@@ -1,10 +1,21 @@
 import React, { PropTypes, Component } from 'react';
 import { Navigator } from 'react-native';
+import { SpashContainer } from '~/containers';
 
 export default class ReactModoroNavigator extends Component {
+  renderScene = (route, navigator) => {
+    <SplashContainer navigator={navigator} />
+  }
+  configureScene = (route) => {
+
+  }
+
   render () {
     return (
-      <Navigator />
-    )
+      <Navigator 
+        renderScene={this.renderScene}
+        configureScene={this.configureScene}
+      />
+    );
   }
 }
