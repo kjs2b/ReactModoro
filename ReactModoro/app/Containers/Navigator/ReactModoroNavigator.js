3918f2c7eb0cd6874992162f27f4249903ee75be
Kevin <kjs2b@virginia.edu>
2017-04-25 14:17:34 -0700

    Set up Navigator

---
+++ ReactModoro/app/Containers/Navigator/ReactModoroNavigator.js
@@ -0,0 +1,10 @@
+import React, { PropTypes, Component } from 'react';
+import { Navigator } from 'react-native';
+
+export default class ReactModoroNavigator extends Component {
+  render () {
+    return (
+      <Navigator />
+    )
+  }
+}
---
+++ ReactModoro/app/Containers/index.js
@@ -0,0 +1,1 @@
+export ReactModoroNavigator from './Navigator/ReactModoroNavigator';
