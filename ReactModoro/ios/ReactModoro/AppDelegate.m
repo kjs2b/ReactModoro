4e05f96aefc35468b8554c258667c7c7f403067a
Kevin <kjs2b@virginia.edu>
2017-04-25 12:16:55 -0700

    ios Facebook SDK setup complete

--- ReactModoro/ios/ReactModoro/AppDelegate.m
+++ ReactModoro/ios/ReactModoro/AppDelegate.m
@@ -35,4 +35,17 @@
   return YES;
 }
 
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+  [FBSDKAppEvents activateApp];
+}
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+  return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                         openURL:url
+                                               sourceApplication:sourceApplication
+                                                      annotation:annotation];
+}
+
 @end
